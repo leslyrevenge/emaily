0d6f8b671f305c439832cbf2d37979006daa8185
Lesly Revenge <leslyrevenge@gmail.com>
2018-02-24 16:36:29 -0500

    dev environment no errors

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,9 +12,9 @@ module.exports = (app) => {
     );
 
     app.get( '/auth/google/callback', passport.authenticate('google'));
-    app.get('/api/logout', () => {
+    app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(request.user);
+        res.send(req.user);
     });
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
--- services/passport.js
+++ services/passport.js
@@ -9,7 +9,7 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((user, done) => {
+passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
         done(null, user);
     })
