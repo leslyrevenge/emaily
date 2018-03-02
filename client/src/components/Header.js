db13e13f3dffe41af63bd5073f9b7320d1b01659
Lesly Revenge <leslyrevenge@gmail.com>
2018-03-02 05:12:44 -0500

    start point for current_user error, no res.data is being fetch. Bad communication between the two servers

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,37 @@
 import React, {Component } from 'react';
+import { connect } from 'react-redux'
 
 class Header extends Component {
+    renderContent(){
+        switch (this.props.auth) {
+            case null:
+                return 'still deciding'
+            case false: 
+                return "still deciding"
+            default: 
+                return 'im logged in'
+        }
+    }
     render(){
+        console.log(this.props)
         return(
             <div>
-                Header
+                <nav>
+                    <div className="nav-wrapper">
+                    <a  className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                       {this.renderContent}
+                    </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
 }
 
-export default Headerserver
+function mapStateToProps( { auth }){
+    return { 
+        auth
+    }
+}
+export default connect(mapStateToProps)(Header);
